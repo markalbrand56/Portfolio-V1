3b457e7388dad561dad5b283f5fe9735ce669ae3
Mark Albrand <mark.albrand5@gmail.com>
2023-05-23 00:20:55 -0600

    Style

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import React from "react"
 import "./App.css"
 import Proyecto from "./components/Proyecto/Proyecto"
+import projects from "./assets/projects"
 
 function App() {
     return (
         <div className="App">
             <p>Prueba</p>
-            <Proyecto
-                title="Reading Planner API"
-                tags={["Go", "MongoDB", "Gin Gonic"]}
-                url="https://github.com/markalbrand56/Reading-Planner-API"
-                description="REST API for Reading-Planner app using MongoDB and Go The project's objective is to track the user on the books that are currently being read and the ones that are on the queue. This API will follow the REST design pattern, due to its low complexity overall."
-                icon=""
-            />
+            {projects.map((project) => (
+                <Proyecto
+                    key={project.title}
+                    title={project.title}
+                    description={project.description}
+                    tags={project.tags}
+                    url={project.url}
+                />
+            ))}
         </div>
     )
 }
