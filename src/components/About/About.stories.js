d78fc191d36a00bd4364426174b07eb704f500fb
Mark Albrand <mark.albrand5@gmail.com>
2023-06-01 16:26:04 -0600

    Correction

--- src/components/About/About.stories.js
+++ src/components/About/About.stories.js
@@ -9,8 +9,8 @@ export default {
 
 export const AboutMe = {
     args: {
-        about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget ultricies",
-        picture: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.motor.es%2Fdodge%2Fcharger%2F&psig=AOvVaw0YUz8OWsHLvNQ5rwvusZIF&ust=1685740825993000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNC9w4uAo_8CFQAAAAAdAAAAABAE",
+        about: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget ultricies"],
+        picture: "https://cdn-images.motor.es/image/m/756w504h.webp/fotos-jato/dodge/uploads/dodge-charger-6362ee8e3fab3.jpg",
     },
 }
 
